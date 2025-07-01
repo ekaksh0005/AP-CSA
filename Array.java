e43b5407f7ecb5d5fe17da10a5999bc7e7056d31
ekaksh0005 <ekaksh1031@gmail.com>
2025-07-02 01:10:42 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,30 @@
+/*import java.util.*;
+public class Array
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Enter 10 numbers");
+        int b[]=new int[10];
+        for(int i=0; i<10; i++)
+        {
+            b[i]=sc.nextInt();
+        }
+        for(int i=0; i<10;i++)
+        {
+         System.out.println(b[i]);   
+        }
+    }
+}*/
+
+public class Array
+{
+  public static void main (String[] args)
+  {
+      int i;
+      char m[]={'c','o','m','p','u','t','e','r'};
+      System.out.println("The characters of the array elements are:");
+      for (i=0;i<8;i++)
+          System.out.println(m[i]);
+  }
+}
