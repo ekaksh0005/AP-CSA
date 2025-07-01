299bdb52d0583ddbc00d5982cd40bc9fc1003d35
ekaksh0005 <ekaksh1031@gmail.com>
2025-07-02 01:13:42 +0530

    Create AP2.java

---
+++ AP2.java
@@ -0,0 +1,12 @@
+/*public class AP2
+{
+    public static void main(String[] args)
+    {
+        Double score1= new Double(89.45);
+        System.out.println("Score1 :" + score1);
+        Double score2= new Double(92.38);
+        Double score3= new Double(101.0);
+        Double meanScore= (score1 + score2 + score3) /3;
+        System.out.println("Double meanScore : " + meanScore);
+    }
+}*/
