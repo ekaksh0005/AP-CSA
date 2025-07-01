4ba92006ec9cd94ed73bb43aa0ac774c88e78a29
ekaksh0005 <ekaksh1031@gmail.com>
2025-07-02 01:09:55 +0530

    update

---
+++ AP.java
@@ -0,0 +1,72 @@
+/*import java.util.*;
+public class AP
+{
+    public static void main(String args[])
+    {
+        Scanner in= new Scanner
+        System.out.println("Enter integer");
+        int a;
+        a=in.nextInt();
+        System.out.println(a);
+        System.out.println("Enter a decimal");
+        double b;
+        b=in.nextDouble();
+        System.out.print(b);
+        
+        
+        
+    }
+} */
+
+/*public class AP
+{
+    public static void main(String [] args)
+    {
+        int myAge=20;
+        System.out.println("Current age:" + myAge);
+        if (myAge >=16)
+        {
+            System.out.println("You can learn to drive a car");
+            myAge++;
+        }
+        System.out.println("On your next birthday, you will be" + myAge + ".");
+    }
+}*/
+
+//WAP to input 3 numbers and display the greatest number.
+/*import java.util.*;
+public class AP
+{
+    public static void main(String [] args)
+    {
+        Scanner in= new Scanner(System.in);
+        int first,second,third,d,p;
+        System.out.println("Enter 3 nos.");
+        first=in.nextInt();
+        second=in.nextInt();
+        third=in.nextInt();
+        d=Math.max(first,second);
+        p=Math.max(third,d);
+        System.out.println("Greatest number: " +p);
+    }
+}*/
+
+// WAP to calculate the diagonal of a cuboid taking length, breadth and height as input. 
+/*import java.util.*;
+public class AP
+{
+    public static void main(String [] args)
+    {
+        Scanner in= new Scanner(System.in);
+        int length, breadth, height;
+        double d;
+        System.out.println("Enter length, breadth and height of a cuboid");
+        length=in.nextInt();
+        breadth=in.nextInt();
+        height=in.nextInt();
+        d=Math.sqrt(length*length+breadth*breadth+height*height);
+        System.out.println("Diagonal of a cuboid= " +d);
+        
+    }
+}*/
+
